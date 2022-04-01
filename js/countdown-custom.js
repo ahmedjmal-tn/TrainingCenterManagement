b5f7712013969789d9f3c8e6a8cc8f2acb039018
ASUS@ahmed <ahmed.jemel2639@gmail.com>
2022-04-01 19:31:01 +0100

    TrainingCenterManagement

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date("16 mars 2022 00:01:00")}); // year, month, date, hour
+        });
+});		
+
---
+++ readme.txt
@@ -0,0 +1,1 @@
+The link of my website : https://larousse2001.github.io/TrainingCenterManagement/
